1a3c945123f192d7c5d99e2b165332709c564a13
Dauren Shalabayev <dauren.shalabayev@gmail.com>
2025-08-09 20:09:55 +0500

    vv

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,15 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    rollupOptions: {
+      external: [],
+    },
+    commonjsOptions: {
+      include: [/node_modules/],
+    },
+  },
+  optimizeDeps: {
+    include: ['axios']
+  }
 })
